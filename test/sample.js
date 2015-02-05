0ed3c799f1d5f16e04a4b72aa30ff1216f2566c7
“Sumit <er.sumitarora@gmail.com>
2015-02-04 21:48:54 -0500

    updating clients api

--- test/sample.js
+++ test/sample.js
@@ -1,9 +1,27 @@
+var superagent = require('superagent')
+var expect = require('expect.js')
 var assert = require("assert");
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
-        });
+
+describe('express rest api server', function() {
+  it('retrieves a collection', function(done) {
+    superagent.get('http://localhost:3000/collections/test')
+      .end(function(e, res) {
+        console.log(res.body)
+        expect(e).to.eql(null)
+        expect(res.body.length).to.be.above(0)
+        expect(res.body.map(function(item) {
+          return item._id
+        })).to.contain(id)
+        done()
+      })
+  })
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1, 2, 3].indexOf(5));
+      assert.equal(-1, [1, 2, 3].indexOf(0));
     });
+  });
 });
