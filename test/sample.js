4cb624cf2a0c16bc3d06102bf06e1e87778be9c4
“Sumit <er.sumitarora@gmail.com>
2015-02-06 21:01:31 -0500

    fixing build

--- test/sample.js
+++ test/sample.js
@@ -2,21 +2,6 @@ var superagent = require('superagent')
 var expect = require('expect.js')
 var assert = require("assert");
 
-describe('express rest api server', function() {
-  it('retrieves a collection', function(done) {
-    superagent.get('http://localhost:3000/collections/test')
-      .end(function(e, res) {
-        console.log(res.body)
-        expect(e).to.eql(null)
-        expect(res.body.length).to.be.above(0)
-        expect(res.body.map(function(item) {
-          return item._id
-        })).to.contain(id)
-        done()
-      })
-  })
-});
-
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
